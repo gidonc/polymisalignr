dd3ebb09a63771369a20a60edd340fb28d04d455
Gidon Cohen <gidon.cohen@durham.ac.uk>
2019-04-23 06:57:57 +0100

    initial commit

---
+++ R/polymisalignr-package.R
@@ -0,0 +1,16 @@
+#' The 'polymisalignr' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name polymisalignr-package
+#' @aliases polymisalignr
+#' @useDynLib polymisalignr, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
+#'
+NULL
